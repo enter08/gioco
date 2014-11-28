e27b16283e6629233c56ac5039afe6fa0fb27e43
Redzep Demic <demicredzep1990@gmail.com>
2014-11-28 16:30:25 +0100

    rakes_generator fixed

--- lib/generators/gioco/rakes_generator.rb
+++ lib/generators/gioco/rakes_generator.rb
@@ -18,7 +18,7 @@ namespace :gioco do
                         #{"points: \'\#\{args.points\}\'," if options[:points]}
                         #{"kind_id: kind.id," if options[:kinds]}
                         default: \'\#\{arg_default\}\'
-                      }, without_protection: true))
+                      }, without_protection: true)
                     else
                       raise 'There is another badge with this name related with this kind'
                     end\n"
@@ -27,7 +27,7 @@ namespace :gioco do
         badge_string = badge_string + "resources.each do |r|
         #{
         if options[:points] && options[:kinds]
-            "r.points  << Point.create({ :kind_id => kinds.id, :value => \'\#\{args.points\}\'}, without_protection: true))"
+            "r.points  << Point.create({ :kind_id => kinds.id, :value => \'\#\{args.points\}\'}, without_protection: true)"
         elsif options[:points]
           "r.points = \'\#\{args.points\}\'"
         end
